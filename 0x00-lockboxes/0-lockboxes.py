b0dfe0051d95abb10249b874d9487ec55b57de28
santiagopinzonD <santiagopinzon456@gmail.com>
2021-04-20 03:57:17 +0000

    lockboxes tasks

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Method that determines if all the boxes can be opened."""
+
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened."""
+
+    keys = [0]
+
+    for index in keys:
+        for key in boxes[index]:
+            if key not in keys and key < len(boxes):
+                keys.append(key)
+    print(keys)
+    if len(keys) == len(boxes):
+        return True
+    return False
---
+++ 0x00-lockboxes/README.md
@@ -0,0 +1,1 @@
+loock boxes
