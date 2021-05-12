f129473b502d021b3d1a1d3481e74690ec0143cc
santiagopinzonD <santiagopinzon456@gmail.com>
2021-05-11 20:06:20 -0500

    edit logic

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,27 +1,49 @@
+#include <stdlib.h>
 #include "lists.h"
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list.
- * @n: nodes
- * @head: first node
- * Return: New node.
- */
-listint_t *insert_node(listint_t **head, int number);
-listint_t *add_nodeint(listint_t **head, const int n)
+ * insert_node -add node to list.
+ * Description: Function that inserts a number into a sorted singly linked list
+ * @head: pointer to a pointer, head of the list
+ * @number: integer value to insert
+ * Return: the address of the new node, or NULL if it failed
+ **/
+listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
+	listint_t *newNode, *tmp = *head;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
 		return (NULL);
-	}
-	if (new)
+	newNode->n = number;
+	if (*head == NULL) 
 	{
-		new->number = number;
-		new->next = *head;
-		*head = new;
-		return (new);
+		*head = newNode;
+		newNode->next = NULL;
+		return (newNode);
 	}
-	free(new);
-	return (NULL);
+	if (tmp->next == NULL)
+	{
+		newNode->next = NULL;
+		tmp->next = newNode;
+		return (newNode);
+	}
+	if (number <= tmp->n)
+	{
+		newNode->next = tmp;
+		*head = newNode;
+		return (newNode);
+	}
+	while (tmp->next != NULL)
+	{
+		if (number <= tmp->next->n)
+		{
+			newNode->next = tmp->next;
+			tmp->next = newNode;
+			return (newNode);
+		}
+		tmp = tmp->next;
+	}
+	tmp->next = newNode;
+	newNode->next = NULL;
+	return (newNode);
 }
