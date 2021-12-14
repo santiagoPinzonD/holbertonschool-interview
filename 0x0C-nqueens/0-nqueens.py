b5372c6f91e100fc7149e3d6fdb09c9e05b45c7a
santiagoPinzonD <santiagopinzon456@gmail.com>
2021-12-13 21:05:37 -0500

    modify file

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,68 +1,37 @@
 #!/usr/bin/python3
-"""Solution to N Queens problem"""
+""" Module that solves the N queens problem """
+
+
 import sys
 
 
-def format(board):
-    """Prints according to requirements"""
-    ret = []
-    for i in range(len(board)):
-        colIdx = board[i].index(1)
-        ret.append([i, colIdx])
-    print(ret)
-
-
-def isValid(board, curCol, row, n):
-    """Checks if board[row][curCol] is a valid queen"""
-    # Check prev columns
-    for i in range(curCol):
-        if board[row][i] == 1:
-            return False
-    # Check for upper diagonal
-    i = row
-    j = curCol
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j -= 1
-    # Check for lower diagonal
-    i = row
-    j = curCol
-    while i < n and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i += 1
-        j -= 1
-    return True
-
-
-def nQueens(board, curCol, n):
-    """Recursive call that places queens in all the
-    posible positions of the board"""
-    stat = False
-    if curCol == n:
-        format(board)
-        return True
-    for row in range(0, n):
-        if isValid(board, curCol, row, n):
-            board[row][curCol] = 1
-            stat = nQueens(board, curCol + 1, n) or stat
-            board[row][curCol] = 0
-    return stat
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-    if not sys.argv[1].isdigit():
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
         print("N must be a number")
-        exit(1)
-    n = int(sys.argv[1])
-    if n < 4:
+        sys.exit(1)
+
+    if N < 4:
         print("N must be at least 4")
-        exit(1)
-    board = [[0 for i in range(n)]for j in range(n)]
-    # first col is 0
-    nQueens(board, 0, n)
+        sys.exit(1)
+
+
+def bTracking(N, i=0, a=[], b=[], c=[]):
+    """Generates backtracking of the answers of the puzzle """
+    if i < N:
+        for j in range(N):
+            if j not in a and i+j not in b and i-j not in c:
+                for ans in bTracking(N, i+1, a+[j], b+[i+j], c+[i-j]):
+                    yield ans  # iterator of the solutions
+    else:
+        yield a  # iterator of the first list with no answer
+
+
+for ans in bTracking(N):
+    resp = [[col, row] for col, row in enumerate(ans)]
+    print(resp)
